d96030433e84a381d8bace49cc263ca6889659de
yuhaitao <yuhaitao1@lczq.com>
2024-08-19 16:22:10 +0800

    modify: 增加星级评分功能

--- StudyiOS/Content/ViewController/HomeViewController/HomeViewController.m
+++ StudyiOS/Content/ViewController/HomeViewController/HomeViewController.m
@@ -53,6 +53,7 @@
                      @{Tab_Title:@"20、苹果系统自带分享功能",Tab_Url:@"AppleShareViewController"},
                      @{Tab_Title:@"21、判断手机是否越狱",Tab_Url:@"PrisonBreakViewController"},
                      @{Tab_Title:@"22、倒计时功能",Tab_Url:@"CountDownViewcontroller"},
+                     @{Tab_Title:@"23、星级评分功能",Tab_Url:@"StarStarViewController"},
     ];
     [self.tableView reloadData];
 }
---
+++ StudyiOS/Content/ViewController/StarStarViewController/StarStarViewController.h
@@ -0,0 +1,16 @@
+//
+//  StarStarViewController.h
+//  StudyiOS
+//
+//  Created by Apple on 2024/8/19.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StarStarViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
