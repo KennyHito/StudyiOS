6165eeb07ffeb15cd9a2a873dd0055795656ae43
yuhaitao <yuhaitao1@lczq.com>
2025-01-23 09:32:05 +0800

    modify: 更新视频url地址

--- StudyiOS/Header/DefineHeader.h
+++ StudyiOS/Header/DefineHeader.h
@@ -11,7 +11,7 @@
 
 #define VersionCache            @"VersionCache"
 
-#define MP4_URL                 @"http://static.tripbe.com/videofiles/20121214/9533522808.f4v.mp4"
+#define MP4_URL                 @"https://vdept3.bdstatic.com/mda-nm3fmpgi9im026tj/sc/cae_h264_mcsic/1670229418769480124/mda-nm3fmpgi9im026tj.mp4?v_from_s=hkapp-haokan-hbe&auth_key=1737606316-0-0-20219be1fc5889bfccc578bb81be538b&bcevod_channel=searchbox_feed&cr=2&cd=0&pd=1&pt=3&logid=1516705133&vid=326432808577015124&klogid=1516705133&abtest="
 
 #define Begin_Guidance_Type     @"BeginGuidanceType"
 
