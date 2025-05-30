e4b9278691b752aced1cb022ae0b35ab88f99493
yuhaitao <yuhaitao1@lczq.com>
2025-05-30 15:51:59 +0800

    modify: 完善功能

--- StudyiOS/Content/ViewController/MovieViewController/MovieViewController.m
+++ StudyiOS/Content/ViewController/MovieViewController/MovieViewController.m
@@ -11,11 +11,13 @@
 #import <AVFoundation/AVFoundation.h>
 
 @interface MovieViewController ()
-
 @property (nonatomic, strong) AVPlayer * player;
 @property (strong, nonatomic) AVPlayerLayer * layer;
 @property (strong, nonatomic) AVPlayerItem *playerItem;
 @property (strong, nonatomic) id playerItemObserver;
+
+@property (nonatomic, strong) UILabel *errorLabel;
+@property (nonatomic, strong) UIActivityIndicatorView *loadingIndicator;
 @end
 
 @implementation MovieViewController
@@ -31,6 +33,13 @@
         [self.player pause];
         self.player = nil;
     }
+    
+    if(self.playerItem){
+        [self.playerItem removeObserver:self forKeyPath:@"status"];
+        [self.playerItem removeObserver:self forKeyPath:@"loadedTimeRanges"];
+        [self.playerItem removeObserver:self forKeyPath:@"playbackBufferEmpty"];
+        [self.playerItem removeObserver:self forKeyPath:@"playbackLikelyToKeepUp"];
+    }
 }
 
 - (void)pageActivityStart{
@@ -39,7 +48,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = [UIColor blackColor];
     [self.player play];//进来直接默认播放
     [self setUpUI];
     [self monitorPlaybackProgress];
@@ -73,6 +82,21 @@
     [UIView animateWithDuration:3.0 animations:^{
         enterMainButton.alpha = 1.0;
     }];
+    
+    // 添加加载指示器
+    self.loadingIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    self.loadingIndicator.center = self.view.center;
+    [self.loadingIndicator startAnimating];
+    [self.view addSubview:self.loadingIndicator];
+    
+    // 添加错误提示标签
+    self.errorLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+    self.errorLabel.center = self.view.center;
+    self.errorLabel.textAlignment = NSTextAlignmentCenter;
+    self.errorLabel.textColor = [UIColor redColor];
+    self.errorLabel.numberOfLines = 0;
+    self.errorLabel.hidden = YES;
+    [self.view addSubview:self.errorLabel];
 }
 
 #pragma mark -- 按钮点击
@@ -94,12 +118,77 @@
 
 #pragma mark -- 监听播放进度
 - (void)monitorPlaybackProgress{
+    [self.playerItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
+    [self.playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [self.playerItem addObserver:self forKeyPath:@"playbackBufferEmpty" options:NSKeyValueObservingOptionNew context:nil];
+    [self.playerItem addObserver:self forKeyPath:@"playbackLikelyToKeepUp" options:NSKeyValueObservingOptionNew context:nil];
+    
     self.playerItemObserver = [[NSNotificationCenter defaultCenter] addObserverForName:AVPlayerItemDidPlayToEndTimeNotification object:self.playerItem queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
         // 当视频播放结束时，将播放位置重置到开始处
         [self.player seekToTime:kCMTimeZero];
         // 重新开始播放
         [self.player play];
     }];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    AVPlayerItem *playerItem = (AVPlayerItem *)object;
+    
+    if ([keyPath isEqualToString:@"status"]) {
+        switch (playerItem.status) {
+            case AVPlayerStatusReadyToPlay:
+                [_loadingIndicator stopAnimating];
+                _errorLabel.hidden = YES;
+                break;
+                
+            case AVPlayerStatusFailed:
+                [_loadingIndicator stopAnimating];
+                [self showError:[NSString stringWithFormat:@"播放失败: %@", playerItem.error.localizedDescription] withDone:NO];
+                break;
+                
+            case AVPlayerStatusUnknown:
+                [self showError:@"视频状态未知" withDone:NO];
+                break;
+        }
+    } else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
+        // 计算缓冲进度
+        NSArray *loadedTimeRanges = [playerItem loadedTimeRanges];
+        CMTimeRange timeRange = [loadedTimeRanges.firstObject CMTimeRangeValue];
+        float startSeconds = CMTimeGetSeconds(timeRange.start);
+        float durationSeconds = CMTimeGetSeconds(timeRange.duration);
+        NSTimeInterval loadedDuration = startSeconds + durationSeconds;
+        
+        // 总时长
+        NSTimeInterval totalDuration = CMTimeGetSeconds(playerItem.duration);
+        
+        NSLog(@"缓冲进度: %.2f%%", (loadedDuration / totalDuration) * 100);
+        // 显示缓冲进度 (这里可以添加进度条UI)
+        NSString *des = KStringWithFormat(@"缓冲进度: %.2f%%", (loadedDuration / totalDuration) * 100);
+        BOOL isDone = (loadedDuration / totalDuration) * 100 >= 100;
+        if (isDone) {
+            des = @"缓存完成";
+        }
+        [self showError:des withDone:isDone];
+        
+    } else if ([keyPath isEqualToString:@"playbackBufferEmpty"]) {
+        if (playerItem.playbackBufferEmpty) {
+            [_loadingIndicator startAnimating];
+        }
+    } else if ([keyPath isEqualToString:@"playbackLikelyToKeepUp"]) {
+        if (playerItem.playbackLikelyToKeepUp) {
+            [_loadingIndicator stopAnimating];
+        }
+    }
+}
+
+- (void)showError:(NSString *)errorMessage withDone:(BOOL)isDone{
+    self.errorLabel.text = errorMessage;
+    self.errorLabel.hidden = NO;
+    if (isDone) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            self.errorLabel.hidden = YES;
+        });
+    }
 }
 
 - (AVPlayer *)player{
@@ -116,10 +205,14 @@
 
 - (AVPlayerItem *)playerItem{
     if(!_playerItem){
-        //选择本地的视频
-        NSURL *movieURL = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"qidong" ofType:@"mp4"]];
-        //网络视频
-        //movieURL = [NSURL URLWithString:MP4_URL];
+        //通过随机来播放本地视频还是网络视频
+        int x = arc4random()%2;
+        //网络视频url
+        NSURL *movieURL = [NSURL URLWithString:MP4_URL];
+        if (x == 1) {
+            //选择本地的视频
+            movieURL = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"qidong" ofType:@"mp4"]];
+        }
         _playerItem = [AVPlayerItem playerItemWithURL:movieURL];
     }
     return _playerItem;
--- StudyiOS/Header/DefineHeader.h
+++ StudyiOS/Header/DefineHeader.h
@@ -11,7 +11,9 @@
 
 #define VersionCache        @"VersionCache"
 
-#define MP4_URL     @"https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-app-video-courses.mp4"
+#define MP4_URL     @"http://baobab.kaiyanapp.com/api/v1/playUrl?vid=330054&resourceType=video&editionType=default&source=aliyun&playUrlType=url_oss&udid="
+
+#define MP4_URL_2     @"https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-app-video-courses.mp4"
 
 #define Begin_Guidance_Type     @"BeginGuidanceType"
 
